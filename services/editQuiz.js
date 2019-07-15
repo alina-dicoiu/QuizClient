0a7f9adda080bc1a1f3c2644da1c5bbdb5197263
ComsaAndreea <andre.comsa@yahoo.com>
2019-07-15 10:28:15 +0300

    Delete Question

--- services/editQuiz.js
+++ services/editQuiz.js
@@ -1,4 +1,4 @@
-var editQuiz = (function() {
+var editQuiz = (function () {
     function loadCategories(Id) {
         $.ajax("https://localhost:44356/api/Categories/ById/" + Id, {
             method: "GET",
@@ -8,12 +8,39 @@ var editQuiz = (function() {
                 $("#categoryTitle").html(data.Name);
             })
 
-        }
+    }
+
+
+    $(document).on("click", ".delete-post-button", function () {
+
+        let questionID = this.getAttribute("data-id");
+
+        $("#delete-question-button").off("click").on("click", function () {
+           deleteQuestion(questionID);
+        })
+
+    });
 
     return {
-        loadCategory : loadCategories,
-        loadQuestions : loadQuestions
+        loadCategory: loadCategories,
+        loadQuestions: loadQuestions
     }
+
+    function deleteQuestion(id){
+    return $.ajax("https://localhost:44356/api/Questions/Delete/" + id,{
+        method: "DELETE",
+        dattaType: "json"
+    })
+        .done(function (data, status, jqXHR){
+            $(".card[data-id="+ id + "]").remove();
+            $("#delete-question-modal").modal('toggle');
+        })
+        .fail(function (jqXHR, status, error){
+            console.log(error);
+        })
+}
+
+
 
     function loadQuestions(Id) {
         $.ajax("https://localhost:44356/api/Questions/ByCategory/" + Id, {
@@ -21,30 +48,30 @@ var editQuiz = (function() {
             dataType: "json"
         })
             .done(function (data, status, jqXHR) {
-                for (let i=0; i<data.length ; i++)
-                $("#questionsContainer").append(newCardForQuestion(data[i]));
+                for (let i = 0; i < data.length; i++)
+                    $("#questionsContainer").append(newCardForQuestion(data[i]));
                 debugger;
             })
             .fail(function (jqXHR, status, error) {
                 $("#questionsContainer").append(newErrorMessage());
             });
 
-        }
+    }
 
-        function newErrorMessage(){
-            return `
+    function newErrorMessage() {
+        return `
             <div class="alert alert-danger categories-error" role="alert">
                 Could not load questions from server.
             </div>`;
-        }
+    }
 
-        function newCardForQuestion(Question){
+    function newCardForQuestion(Question) {
         let Answers = "";
-        for(let i=0; i<Question.PossibleAnswers.length; i++){
+        for (let i = 0; i < Question.PossibleAnswers.length; i++) {
             Answers += newListItem(Question.PossibleAnswers[i]);
         }
-        return`
-            <div class="card">
+        return `
+            <div class="card"  data-id=${Question.Id}>
             <div class="card-header">
                 ${Question.Text}
             </div>
@@ -55,19 +82,23 @@ var editQuiz = (function() {
             </div>
             <div class="card-footer">
                 <button class="btn btn-primary edit-post-button" data-toggle="modal" data-target="#edit-post-modal" data-id=${Question.Id}>Edit</button>
-                <button class="btn btn-danger delete-post-button" data-id=${Question.Id}>Delete</button>
+                <button class="btn btn-danger delete-post-button" data-toggle="modal" data-target="#delete-question-modal" data-id=${Question.Id}>Delete</button>
             </div>
             </div>
             
             `;
-        }
+    }
 
-        function newListItem(Answer){
-            let styleClass = "incorrect";
-            if(Answer.Correct == true)
-                styleClass = "correct";
-            return `<li class="list-group-item ${styleClass}">${Answer.Text}</li>`
-        }
 
-})(); 
+
+
+
+    function newListItem(Answer) {
+        let styleClass = "incorrect";
+        if (Answer.Correct == true)
+            styleClass = "correct";
+        return `<li class="list-group-item ${styleClass}">${Answer.Text}</li>`
+    }
+
+})();
 
